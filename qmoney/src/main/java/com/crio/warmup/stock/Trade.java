4903b2ebce23fac0244efcc7e8aefeaa8b0a0d09
Harshit221 <harshitvaghani221@gmail.com>
2020-02-22 10:36:09 +0000

    ME_QMONEY_MODULE_JSON_PARSING

---
+++ qmoney/src/main/java/com/crio/warmup/stock/Trade.java
@@ -0,0 +1,51 @@
+package com.crio.warmup.stock;
+
+public class Trade {
+    String sybmol;
+    int quantity;
+    String tradeType;
+    String purchaseDate;
+
+    public Trade() {
+    }
+
+    public Trade(String sybmol, int quantity, String tradeType, String purchaseDate) {
+        this.sybmol = sybmol;
+        this.quantity = quantity;
+        this.tradeType = tradeType;
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getSybmol() {
+        return sybmol;
+    }
+
+    public void setSybmol(String sybmol) {
+        this.sybmol = sybmol;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getTradeType() {
+        return tradeType;
+    }
+
+    public void setTradeType(String tradeType) {
+        this.tradeType = tradeType;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+}
